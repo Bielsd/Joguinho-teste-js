a2e5182840ceee0d4e0a48adbc35a293b1586ad2
Bielsd <slafer_gabriel@hotmai.com>
2018-06-10 21:59:30 -0300

    trabalhando na primeira fase do jogo, precisa ajustar o texto escondido

---
+++ Fase1/fase1.js
@@ -0,0 +1,20 @@
+let quebrada = false;
+let textoaparece = false;
+
+function mudaLampada (tipo){
+    if (!quebrada){
+        document.getElementById("Luz").src= "imagens/" + tipo + ".jpg";
+        if (tipo =='lampada-quebrada'){
+            quebrada = true;
+            if (quebrada == true){
+                textoaparece == true
+                if (textoaparece == true){
+                    getElementById("texto1")
+                }
+            }
+                
+            
+        }
+    }
+      
+}
